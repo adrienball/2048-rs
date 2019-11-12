7cf347224a703502ed3a3a80c22c7b25442e8ad0
Adrien Ball <>
2019-11-12 20:37:16 +0100

    Add PrecomputedEvaluator

--- src/evaluators.rs
+++ src/evaluators.rs
@@ -7,6 +7,55 @@ pub trait BoardEvaluator {
     fn evaluate(&self, board: Board) -> f32;
 }
 
+/// Evaluate a `Board` by evaluating independently each row and column and adding the results
+pub trait RowColumnEvaluator {
+    fn evaluate_row(&self, row: u16) -> f32;
+}
+
+impl<T> BoardEvaluator for T
+where
+    T: RowColumnEvaluator,
+{
+    fn evaluate(&self, board: Board) -> f32 {
+        (0..4)
+            .into_iter()
+            .map(|i| self.evaluate_row(board.get_row(i)) + self.evaluate_row(board.get_column(i)))
+            .sum()
+    }
+}
+
+/// `BoardEvaluator` implementation which encapsulates a `RowColumnEvaluator` and pre-computes
+/// values for any possible row / column. These values are stored in a simple vector which is
+/// indexed by the `u16` representation of each row.
+pub struct PrecomputedEvaluator {
+    row_cache: Vec<f32>,
+}
+
+impl PrecomputedEvaluator {
+    pub fn new<T>(evaluator: T) -> Self
+    where
+        T: RowColumnEvaluator,
+    {
+        let row_cache = (0..(std::u16::MAX as usize + 1))
+            .into_iter()
+            .map(|row| evaluator.evaluate_row(row as u16))
+            .collect();
+        Self { row_cache }
+    }
+}
+
+impl BoardEvaluator for PrecomputedEvaluator {
+    fn evaluate(&self, board: Board) -> f32 {
+        (0..4)
+            .into_iter()
+            .map(|i| {
+                self.row_cache[board.get_row(i) as usize]
+                    + self.row_cache[board.get_column(i) as usize]
+            })
+            .sum()
+    }
+}
+
 /// A simple implementation of `BoardEvaluator` which evaluates a board by simply computing
 /// the proportion of tiles which are zero.
 pub struct ZeroCountEvaluator;
@@ -28,56 +77,27 @@ impl BoardEvaluator for SumEvaluator {
     }
 }
 
+/// `BoardEvaluator` implementation which computes inversions on rows and columns, add them, and
+/// then takes the negative value of it.
+/// Inversions are computed by summing the differences between exponents which appear in the
+/// wrong order in each row / column
 pub struct InversionEvaluator;
 
-impl BoardEvaluator for InversionEvaluator {
-    fn evaluate(&self, board: Board) -> f32 {
-        let inversion_factor =
-            self.row_inversion_factor(board) + self.column_inversion_factor(board);
-        // 240 = 16 * 15 is the max number of inversions
-        1. - inversion_factor as f32 / 240.
-    }
-}
-
-impl InversionEvaluator {
-    fn row_inversion_factor(&self, board: Board) -> u8 {
-        let mut inversion_factor = 0;
-        for row in 0..4 {
-            let mut left_value = board.get_exponent_value(4 * row);
-            let mut left_right_inversions = 0;
-            let mut right_left_inversions = 0;
-            for col in 1..4 {
-                let v = board.get_exponent_value(4 * row + col);
-                if v < left_value {
-                    left_right_inversions += left_value - v;
-                } else if v > left_value {
-                    right_left_inversions += v - left_value;
-                }
-                left_value = v;
+impl RowColumnEvaluator for InversionEvaluator {
+    fn evaluate_row(&self, row: u16) -> f32 {
+        let mut left_value = row >> 12;
+        let mut left_right_inversions = 0;
+        let mut right_left_inversions = 0;
+        for col in 1..4 {
+            let v = (row >> 4 * (3 - col)) & 0b1111;
+            if v < left_value {
+                left_right_inversions += left_value - v;
+            } else if v > left_value {
+                right_left_inversions += v - left_value;
             }
-            inversion_factor += min(left_right_inversions, right_left_inversions);
+            left_value = v;
         }
-        inversion_factor
-    }
-
-    fn column_inversion_factor(&self, board: Board) -> u8 {
-        let mut inversion_factor = 0;
-        for col in 0..4 {
-            let mut up_value = board.get_exponent_value(col);
-            let mut up_bottom_inversions = 0;
-            let mut bottom_up_inversions = 0;
-            for row in 1..4 {
-                let v = board.get_exponent_value(4 * row + col);
-                if v < up_value {
-                    up_bottom_inversions += up_value - v;
-                } else if v > up_value {
-                    bottom_up_inversions += v - up_value;
-                }
-                up_value = v;
-            }
-            inversion_factor += min(up_bottom_inversions, bottom_up_inversions);
-        }
-        inversion_factor
+        -(min(left_right_inversions, right_left_inversions) as f32)
     }
 }
 
@@ -117,11 +137,29 @@ mod tests {
         let evaluator = InversionEvaluator {};
 
         // When
-        let row_inversions = evaluator.row_inversion_factor(board);
-        let col_inversions = evaluator.column_inversion_factor(board);
-        assert_eq!(19, row_inversions);
-        assert_eq!(24, col_inversions);
+        let row_inversions = evaluator.evaluate_row(board.get_row(1));
+        let col_inversions = evaluator.evaluate_row(board.get_column(1));
         let evaluation = evaluator.evaluate(board);
-        assert_eq!(1. - (24. + 19.) / (15. * 16.), evaluation);
+        assert_eq!(-8., row_inversions);
+        assert_eq!(-7., col_inversions);
+        assert_eq!(-19. - 24., evaluation);
+    }
+
+    #[test]
+    fn test_precomputed_inversion_evaluator() {
+        // Given
+        #[rustfmt::skip]
+        let vec_board = vec![
+            2, 4, 2, 4,
+            8, 256, 0, 512,
+            0, 0, 1024, 4,
+            8, 2, 16, 64,
+        ];
+        let board = Board::from(vec_board);
+        let evaluator = PrecomputedEvaluator::new(InversionEvaluator {});
+
+        // When
+        let evaluation = evaluator.evaluate(board);
+        assert_eq!(-19. - 24., evaluation);
     }
 }
